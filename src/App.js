9e7b2d533d327b6631626ac5e6f43d3449791cd2
Raffay Syed <raffayhusayn@gmail.com>
2022-06-16 22:44:49 -0400

    Add CSS styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Heading from './components/Heading.js';
 import List from './components/List.js';
 import { RotatingLogo } from './components/RotatingLogo';
+import Navbar from './components/Navbar.js';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <RotatingLogo />
       <Heading />
       <List />
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar(){
+    return (
+       <nav className="navbar"> 
+            <ul className="nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+       </nav> 
+    )
+}
+
+export default Navbar;
