6ca2357ac9e61a3b8adcc24f7bf63338f56dc50f
Raffay Syed <raffayhusayn@gmail.com>
2022-06-21 23:43:22 -0400

    Add code to change the nav bar component
    -- Add a react icon
    -- Add a Website name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
+import reactIcon from '../images/react-icon-small.png'
+
+
 function Navbar(){
     return (
        <nav className="navbar"> 
             <ul className="nav-items">
+                <img src={reactIcon} alt='react-small-icon' />
+                <h3>React Facts</h3>
                 <li>Pricing</li>
                 <li>About</li>
                 <li>Contact</li>
--- src/components/RotatingLogo.js
+++ src/components/RotatingLogo.js
@@ -1,4 +1,4 @@
-import logo from './../logo.svg';
+import logo from './../images/logo.svg';
 
 export function RotatingLogo(){
     return <img src={logo} className="App-logo" id='react-logo' alt="logo" />
