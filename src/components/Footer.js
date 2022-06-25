62be0af3ca673e1e963f188a6de1016dfcd31942
Raffay Syed <raffayhusayn@gmail.com>
2022-06-24 22:17:51 -0400

    Add code to change the footer
    -- Add icons
    -- Add links to icons
    -- Add css to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
+import {AiFillTwitterSquare, AiFillFacebook, AiFillInstagram, AiFillGithub} from 'react-icons/ai';
+
 function Footer(){
     return(
         <footer className="Footer">
-            <small>
-                © 2022 Raffay Inc 
-            </small>
+            <a href='https://www.twitter.com'><AiFillTwitterSquare className='footer-icon' /></a>
+            <a href='https://www.facebook.com'><AiFillFacebook className='footer-icon'/></a>
+            <a href='https://www.instagram.com'><AiFillInstagram className='footer-icon'/></a>
+            <a href='https://www.github.com'><AiFillGithub className='footer-icon'/></a>
         </footer>
     )
 }
