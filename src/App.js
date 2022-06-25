e212d05c1596d930ec43738a52b1be6d10bfdc39
Raffay Syed <raffayhusayn@gmail.com>
2022-06-25 19:06:36 -0400

    Add css style to Interests and About components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Footer from './components/Footer.js';
 import Info from './components/Info.js';
 import About from './components/About.js';
+import Interests from './components/Interests.js'
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Info />
       <About />
+      <Interests />
       <Footer />
     </div>
   );
---
+++ src/components/Interests.js
@@ -0,0 +1,11 @@
+const interests = "Fighting. Sword fighting. Pillage. Killing";
+function Interests(){
+    return(
+        <div>
+            <h3 className="body-heading">Interests</h3>
+            <p className="body-text">{interests}</p>
+        </div>
+    )
+}
+
+export default Interests;
